209c1ceaddf6391794a32e346ed2202cebf0ccd9
devclient187 <devclient187@gmail.com>
2021-05-11 16:39:00 +0200

    wp plugin init

---
+++ onlineformular-download-plugin/onlineformular-download-plugin.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * Onlineformular Download
+ *
+ * @package           OnlineformularDownloadPlugin
+ * @author            Philipp Löwen
+ * @copyright         2021 Your Name or Company Name
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Onlineformular Download
+ * Plugin URI:        https://example.com/plugin-name
+ * Description:       Description of the plugin.
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Your Name
+ * Author URI:        https://example.com
+ * Text Domain:       plugin-slug
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
+
+add_action( 'admin_menu', 'wporg_options_page' );
+function wporg_options_page() {
+    add_menu_page(
+        'WPOrg',
+        'WPOrg Options',
+        'manage_options',
+        plugin_dir_path(__FILE__) . 'admin/view.php',
+        null,
+        plugin_dir_url(__FILE__) . 'images/icon_wporg.png',
+        20
+    );
+}
