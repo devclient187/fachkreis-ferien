ff388a04f9f0badd31b3e706b4fa457ec1e31687
devclient187 <devclient187@gmail.com>
2021-05-11 18:08:06 +0200

    temp

--- onlineformular-download-plugin/admin/view.php
+++ onlineformular-download-plugin/admin/view.php
@@ -1,1 +1,18 @@
 <h1>Cool!</h1>
+<?php
+$args = array(
+	'numberposts'	=> 20,
+	'category'		=> 4
+);
+$my_posts = get_posts( $args );
+
+if( ! empty( $my_posts ) ){
+	$output = '<ul>';
+	foreach ( $my_posts as $p ){
+		$output .= '<li><a href="' . get_permalink( $p->ID ) . '">' 
+		. $p->post_title . '</a></li>';
+	}
+	$output .= '<ul>';
+}
+
+echo $output
