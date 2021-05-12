5740fd04af41feaf884630be83e9ef525e43462c
devclient187 <devclient187@gmail.com>
2021-05-12 22:46:59 +0200

    .

--- onlineformular-download-plugin/admin/view.php
+++ onlineformular-download-plugin/admin/view.php
@@ -1,20 +1,20 @@
 <?php
 echo 'Loading...'
-$args = array(
-	'post_type'		=> 'flamingo_inbound'
-);
-$my_posts = get_posts( $args );
+// $args = array(
+// 	'post_type'		=> 'flamingo_inbound'
+// );
+// $my_posts = get_posts( $args );
 
-if( ! empty( $my_posts ) ){
-	// $output = '<ul>';
-	foreach ( $my_posts as $p ){
-		echo $p->post_title . '<br>';
-		// $output .= '<li><a href="' . get_permalink( $p->ID ) . '">' 
-		// . $p->post_title . '</a></li>';
-	}
-	// $output .= '<ul>';
-}
-else {
-	echo 'Keine Posts'
-}
-// echo $output
+// if( ! empty( $my_posts ) ){
+// 	// $output = '<ul>';
+// 	foreach ( $my_posts as $p ){
+// 		echo $p->post_title . '<br>';
+// 		// $output .= '<li><a href="' . get_permalink( $p->ID ) . '">' 
+// 		// . $p->post_title . '</a></li>';
+// 	}
+// 	// $output .= '<ul>';
+// }
+// else {
+// 	echo 'Keine Posts'
+// }
+// // echo $output
